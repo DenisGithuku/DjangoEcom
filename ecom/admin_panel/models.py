72c57edd6f561d328be9210d21e93cf7f38b37de
Denis Githuku <githukudenis@gmail.com>
2023-02-27 10:20:59 +0300

    create product model

--- ecom/admin_panel/models.py
+++ ecom/admin_panel/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    id = models.BigAutoField(primary_key=True, auto_created=True)
+    name = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=200)
+    category = models.CharField(max_length=50)
+    price = models.CharField(max_length=20)
+    img = models.ImageField(upload_to='/images')
