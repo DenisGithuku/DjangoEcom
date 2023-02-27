b54f3722d7a0734cca8da84e80d89b64db1bd40e
Denis Githuku <githukudenis@gmail.com>
2023-02-27 09:33:25 +0300

    create panel skeleton

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('auth_app.urls'))
+    path('auth/', include('auth_app.urls')),
+    path('admin_panel', include('admin_panel.urls'))
 ]
